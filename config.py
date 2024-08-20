1fecf01219ee48f8e9b7ee82ac5c83b589d85732
Nurlan-Aliev <nualiev612@gmail.com>
2024-08-20 15:49:16 +0400

    create connect with DB

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Setting(BaseSettings):
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DEBUG: str = os.getenv("DEBUG", "False")
+
+    @property
+    def db_connect(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+setting = Setting()
---
+++ database.py
@@ -0,0 +1,42 @@
+from asyncio import current_task
+from config import setting
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class DataBaseHelper:
+    def __init__(self, url, echo):
+        self.async_engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.async_session_factory = async_sessionmaker(
+            self.async_engine, autoflush=False, autocommit=False, expire_on_commit=False
+        )
+
+    def get_scoped_session(self):
+
+        return async_scoped_session(
+            self.async_session_factory,
+            scopefunc=current_task,
+        )
+
+    def scope_session_dependency(self):
+        session = self.get_scoped_session()
+        yield session
+        session.close()
+
+
+db_helper = DataBaseHelper(
+    setting.db_connect,
+    setting.DEBUG,
+)
